856e3effab3134afc521e663adee4b9db820d90c
James Elliott <james@deepsymmetry.org>
2020-05-06 22:27:56 -0500

    Add link checking to Netlify build
    Also fix some broken links found during a dry run, and convert to a
    configuration-file driven build.

---
+++ doc/build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# This script is run by Netlify continuous integration to build the
+# Antora site hosting the user guide.
+
+npm i @antora/cli antora-site-generator-lunr
+DOCSEARCH_ENABLED=true DOCSEARCH_ENGINE=lunr $(npm bin)/antora --fetch --generator antora-site-generator-lunr \
+  doc/netlify.yml
